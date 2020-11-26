10fa1e76ddccc48bc189b558e7f450dda7e973e4
aryanmonger20 <56786156+aryanmonger20@users.noreply.github.com>
2020-11-27 02:11:46 +0530

    dbconnect

--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,24 @@ import express from "express"
 import mongoose from "mongoose"
 import Messages from"./dbmessages.js"
 import Pusher from 'pusher'
+import cors from 'cors'
+//const bodyParser=require('body-parser')
+import bodyParser from 'body-parser'
 
 const app=express();
-const port =process.env.PORT||3000
+const port =process.env.PORT||5000
 
+ //middleware
+ 
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+app.use(bodyParser.json())
+ app.use(express.json());
+ app.use(cors());
 
 //dbconfig
-const connection_url="mongodb+srv://aryan:kkU5vaihkG5evGa4@cluster0.gvfde.mongodb.net/chatappdb?retryWrites=true&w=majority";
-
+const connection_url="mongodb+srv://aryan:mohit2009@cluster0.mduhl.mongodb.net/chatapp?retryWrites=true&w=majority";
 mongoose.connect(connection_url,{
     useCreateIndex:true,
     useNewUrlParser:true,
@@ -23,27 +33,38 @@ mongoose.connect(connection_url,{
     cluster: "ap2",
     useTLS: true
   });
-  
-  pusher.trigger("my-channel", "my-event", {
+ 
+//   app.use(req,res,next=>{
+//       res.setHeader("Access-Control-Allow-Origin","*");
+//       res.setHeader("Access-Control-Allow-Headers","*");
+//       next();
+//   })
+
+
+
+  pusher.trigger("messages", "inserted", {
     message: "hello world"
   });
 
 
-  const db=mongoose.connection
+  const db=mongoose.connection;
 
   db.once('open',()=>{
       console.log("DB connected")
       const msgCollection=db.collection('messagecontents')
       const changeStream=msgCollection.watch()
+      
       changeStream.on("change",(change)=>{
           console.log("change occured");
+         
 
           if (change.operationType==='insert'){
               const messageDetails=change.fullDocument
               pusher.trigger('messages','inserted',
               {
                   name:messageDetails.user,
-                  message:messageDetails.message
+                  message:messageDetails.message,
+                  timestamp:messageDetails.timestamp,
               }
               )
           }else{
@@ -66,12 +87,17 @@ app.get('messages/sync',(req,res)=>{
     })
 })
 app.post('/messages/new',(req,res)=>{
-    const dbMessage=req.body
+    const dbMessage=req.body;
+    console.log(dbMessage);
+    console.log(req.body);
+    
     Messages.create(dbMessage,(err,data)=>{
         if(err){
             res.status(500).send(err)
         }else{
             res.status(201).send(data)
+            console.log(data);
+            console.log(dbMessage);
         }
     })
 })
--- whatsapp/src/App.js
+++ whatsapp/src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 import "./App.css";
-
-
+import Pusher from 'pusher-js'
+import axios from "./axios"
 
 function App() {
-  return (
+  const [messages,setMessages]=useState([]);
+
+    useEffect(()=>{
+      axios.get('/messages/sync')
+      .then(response=>{
+        setMessages(response.data);
+      })
+
+    },[])
+    useEffect(()=>{
+      const pusher = new Pusher('e2f0b8495eb7220a67c3', {
+        cluster: 'ap2'
+      });
+  
+      const channel = pusher.subscribe('messages');
+      channel.bind('inserted', function(data) {
+        alert(JSON.stringify(data));
+        console.log(JSON.stringify(data))
+      });
+    },[]);
+
+    console.log(messages);
+    return (
     <div className="app">
      <div className="app_body">
        
